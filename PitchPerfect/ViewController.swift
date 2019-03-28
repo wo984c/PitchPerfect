db5188f2c4caad4c5bb0802fcc7b7d6843a11675
William Ortiz <willam.ortiz@gmail.com>
2019-03-27 22:20:31 -0400

    Initial Commit

--- PitchPerfect/ViewController.swift
+++ PitchPerfect/ViewController.swift
@@ -9,12 +9,35 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    // Changes the label's text when record button was pressed
+    @IBOutlet weak var recordingLabel: UILabel!
+    
+    @IBOutlet weak var recordButton: UIButton!
+    
+    @IBOutlet weak var stopRecordingButton: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
 
+    }
 
+    // Record audio when record button was pressed
+    @IBAction func recordAudio(_ sender: Any) {
+    
+        recordingLabel.text = "Recording in Process ..."
+    }
+    
+    @IBAction func stopRecording(_ sender: Any) {
+    }
 }
 
